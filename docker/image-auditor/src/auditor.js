21436443fa6c9b1b92cf1993b0b2a910bbaa2caf
Le Poulet Suisse <xxpoulletxx@gmail.com>
2016-05-11 13:54:09 +0200

    Labo Done

---
+++ docker/image-auditor/src/auditor.js
@@ -0,0 +1,100 @@
+function Musician(instr, date, addr, p, actSince) {
+    this.instrument = instr;
+    this.uuid = guid();
+    this.lastUpdated = date;
+    this.activeSince = actSince;
+    this.address = addr;
+    this.port = p;
+}
+
+var activeMusician = {};
+
+var protocol = require('./music-protocol');
+var net = require('net');
+
+const instruments = {"ti-ta-ti": "piano", "pouet": "trumpet", "trulu": "flute", "gzi-gzi": "violin", "boum-boum": "drum"};
+
+/*
+ * We use a standard Node.js module to work with UDP
+ */
+var dgram = require('dgram');
+
+/* 
+ * Let's create a datagram socket. We will use it to listen for datagrams published in the
+ * multicast group by musicians and containing the instrument
+ */
+var s = dgram.createSocket('udp4');
+s.bind(protocol.PROTOCOL_PORT, function () {
+    console.log("I'm listening, let's rock baby!!!");
+    s.addMembership(protocol.PROTOCOL_MULTICAST_ADDRESS);
+});
+
+/*
+ * Start a TCP server
+ * */
+net.createServer(function (socket) {
+
+    var musicianToSend = [];
+    Object.keys(activeMusician).forEach(function(key, index){
+        var musicianTmp = {};
+        musicianTmp["uuid"] = this[key].uuid;
+        musicianTmp["instrument"] = this[key].instrument;
+        musicianTmp["activeSince"] = this[key].activeSince;
+        musicianToSend.push(musicianTmp);
+    }, activeMusician);
+
+    // Send the informations to the client
+    socket.write(JSON.stringify(musicianToSend));
+    socket.end();
+}).listen(2205);
+
+/* 
+ * This call back is invoked when a new datagram has arrived.
+ */
+var blacklist = [];
+s.on('message', function (msg, source) {
+    if (blacklist.indexOf(source.address) == -1) {
+        var message = JSON.parse(msg);
+        var sound = message["sound"];
+        if (sound == undefined) {
+            blacklist.push(source.address);
+            console.log("Bad sound ( " + sound + "), that sound ugly!! Ignoring source " + source.address);
+        }
+        var instrumentName = instruments[sound];
+        console.log("Received!! " + instrumentName + ": " + sound + " from address " + source.address);
+        if (activeMusician[source.address]) {
+            activeMusician[source.address].lastUpdated = Date.now();
+        }
+        else {
+            activeMusician[source.address] = new Musician(instrumentName, Date.now(), source.address, source.port, new Date());
+            console.log("New musician! " + instrumentName + " from " + source.address);
+        }
+    }
+    setInterval(deamonCheckDate, 1000);
+
+});
+
+function deamonCheckDate(){
+    Object.keys(activeMusician).forEach(function(key, index){
+        if(Date.now() - this[key].lastUpdated > 10000){
+            console.log("Looks like musician " + key + " with instrument " + this[key].instrument + " is not playing anymore :(");
+            delete this[key];
+        }
+    }, activeMusician);
+}
+
+/*
+ * Found on internet (Thank you stackoverflow!!)
+ * http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+ * */
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+}
+
---
+++ docker/image-musician/src/musician.js
@@ -0,0 +1,62 @@
+var protocol = require('./music-protocol');
+
+/*
+ * We use a standard Node.js module to work with UDP
+ */
+var dgram = require('dgram');
+
+const instruments = {"piano": "ti-ta-ti", "trumpet": "pouet", "flute": "trulu", "violin": "gzi-gzi", "drum": "boum-boum"};
+
+
+/*
+ * Let's create a datagram socket. We will use it to send our UDP datagrams
+ */
+var s = dgram.createSocket('udp4');
+
+function Musician(instrument) {
+
+    this.instrument = instrument;
+
+    Musician.prototype.update = function() {
+        // Do something on update!
+
+        var sonEmis = {
+            sound: instruments[instrument]
+        };
+        var payload = JSON.stringify(sonEmis);
+
+        /*
+         * Finally, let's encapsulate the payload in a UDP datagram, which we publish on
+         * the multicast address. All subscribers to this address will receive the message.
+         */
+        var message = new Buffer(payload);
+        s.send(message, 0, message.length, protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS, function(err, bytes) {
+            console.log("Sending payload: " + payload + " via port " + s.address().port);
+        });
+
+    };
+
+    /*
+     * Let's do a sound each second
+     */
+    setInterval(this.update.bind(this), 1000);
+
+}
+
+var instrument = process.argv[2];
+
+if(instrument == undefined){
+    console.log("USE: docker run -i -t musician {instrument}");
+    return;
+}
+if(instruments[instrument] == undefined){
+    console.log("The specified instrument doesn't exist!")
+    return;
+}
+
+console.log("Musician setted: " + instrument);
+
+/*
+ * This is a new auditor
+ * */
+var musician = new Musician(instrument);
